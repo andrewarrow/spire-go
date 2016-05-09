019cb7ba176c11d6f6470d7b660cf537f26cb65e
Andrew Arrow <andrew@0x7a69.net>
2016-05-09 01:33:58 +0000

    progress

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "github.com/andrewarrow/spire-go/spire"
 
 func main() {
 	fmt.Println("demo")
+	spire.GetDate("")
 }
---
+++ spire/spire.go
@@ -0,0 +1,28 @@
+package spire
+
+import "os"
+import "net/http"
+import "fmt"
+import "io/ioutil"
+import "encoding/json"
+
+func GetDate(date string) {
+	token := os.Getenv("SPIRE_TOKEN")
+	response, err := http.Get("https://app.spire.io/api/events/br?access_token=" +
+		token + "&date=2016-05-09")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bytes))
+	result := make(map[string]interface{})
+	err = json.Unmarshal(bytes, &result)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
